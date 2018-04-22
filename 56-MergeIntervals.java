3874f2411efc4a269e70190012c03dfaa7fc0c36
qizhenghu <springsun19@hotmail.com>
2018-04-22 14:45:30 -0400

    Create 56-MergeIntervals.java

---
+++ 56-MergeIntervals.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
+class Solution {
+    public List<Interval> merge(List<Interval> intervals) {
+      if(intervals == null || intervals.size() <= 0) return intervals;
+      Collections.sort(intervals, new Comparator<Interval>(){
+          public int compare(Interval i1, Interval i2){
+            return i1.start - i2.start;
+          }
+      });
+      List<Interval> results = new ArrayList<Interval>();
+      int start = intervals.get(0).start;
+      int end = intervals.get(0).end;
+      for(Interval i : intervals){
+        if(i.start <= end){
+          end = Math.max(end, i.end);
+        }else{
+        
+          results.add(new Interval(start, end));
+          start = i.start;
+          end = i.end;
+        }
+      }
+      results.add(new Interval(start, end));
+      return results;
+      
+    }
+ }
